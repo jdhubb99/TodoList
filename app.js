01a193f6e8623943daa7000ab6a97afe9489f731
Jacob Hubbard <jacobhubbard@Jacobs-MacBook-Pro.local>
2021-12-27 13:32:25 -0500

    added selectors for input, button, and list; added event listener for button

---
+++ app.js
@@ -0,0 +1,14 @@
+// selectors
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+
+// event listeners
+todoButton.addEventListener("click", addTodo);
+
+// functions
+function addTodo(event) {
+    // prevents form from submitting
+    event.preventDefault();
+    console.log("hello");
+} // addTodo
